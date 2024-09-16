915969914ad7b6d2b062e3b5241fb9f413ae9b99
Huy Quang <huyquang1250@gmail.com>
2024-09-16 20:22:04 +0700

    1st

---
+++ Thermostat.java
@@ -0,0 +1,12 @@
+class thermostat {
+    private float currentTemp; // current temperature
+    private float desiredTemp; // desired temperature
+
+    public void furnace_on() {
+        // turn on furnace
+    }
+
+    public void furnace_off() {
+        // turn off furnace
+    } 
+}
